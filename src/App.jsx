72e02f557ce8c3a9b1cf1c7695ae2d904c83f04c
Jared-Ha <jared.harrison7@gmail.com>
2024-08-05 09:59:55 +0100

    more setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
 
-	return <></>;
+	return <>NC News</>;
 }
 
 export default App;
